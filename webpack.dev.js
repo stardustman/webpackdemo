46f102238a9680f5e036537bb8e3090eede24202
stardust <aoyunyoung@gmail.com>
2019-12-13 10:42:00 +0800

    webpack.dev.js source-map

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,62 @@
 'use strict'
 
+const glob = require('glob')
 const path = require('path')
 const webpack = require('webpack')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const setMPA = () => {
+    const entry = {}
+    const htmlWebpackPlugins = []
+    // 匹配所有的入口文件 index.js
+    const entryFiles = glob.sync(path.join(__dirname,'./src/*/index.js'))
+    console.log('entryFile========>', entryFiles)
+    Object.keys(entryFiles).map((index) => {
+        const entryFile = entryFiles[index]
+        console.log("entry file=====>", entryFile)
+        // 极客时间怎么了，视频教程的正则写的有问题。视频里转义 ( 有错误
+        const match = entryFile.match(/src\/(.*)\/index\.js/)
+        console.log('page name======>', match)
+
+        const pageName = match && match[1]
+        entry[pageName] = entryFile
+
+        htmlWebpackPlugins.push(
+            new HtmlWebpackPlugin({
+               
+                template: path.join(__dirname, `src/${pageName}/index.html`),
+                filename: `${pageName}.html`,
+                chunks: [ pageName],
+                inject: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    preserveLineBreaks: false,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: false
+                }
+            })
+        );
+    })
+    return {
+        entry,
+        htmlWebpackPlugins
+    }
+}
+
+// multi-page app
+// 解构赋值
+const { entry, htmlWebpackPlugins } = setMPA();
+
+
 module.exports = {
     // entry: './src/index.js', // 单一 entry
-    entry: {
-        index: './src/index.js',
-        search: './src/search.js'
-    },
+    // entry: {
+    //     index: './src/index.js',
+    //     search: './src/search.js'
+    // },
+    entry: entry,
     output: {
         path: path.join(__dirname, 'dist'),
         //filename: 'bundle_build_v2.js'
@@ -57,9 +105,10 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new CleanWebpackPlugin()
-    ],
+    ].concat(htmlWebpackPlugins),
     devServer: {
         contentBase: './dist',
         hot: true
-    }
+    },
+    devtool: 'source-map'
 }

f6b71f848d9ff1e9787a10b5c6a210d73cf5ef3b
stardust <aoyunyoung@gmail.com>
2019-12-22 19:35:28 +0800

    server render 加载数据

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const express = require('express');
 
 const { renderToString } = require('react-dom/server');
 const SSR = require('../dist/search-server');
+const fs = require('fs');
+const path = require('path');
+const template = fs.readFileSync(path.join(__dirname,'../dist/search.html'),'utf-8');
+const data = require('./data.json');
 
 const server = (port) => {
     const app = express();
@@ -26,20 +30,24 @@ const server = (port) => {
 
 server(process.env.PORT || 3000);
 const renderMarkup = (str)=>{
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-        ${str}
-    </body>
-    </html>
-    `;
+    // return `
+    // <!DOCTYPE html>
+    // <html lang="en">
+    // <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //     <title>Document</title>
+    // </head>
+    // <body>
+    //     ${str}
+    // </body>
+    // </html>
+    // `;
+
+    const dataStr = JSON.stringify(data);
+    return template.replace('<!--HTML_PLACEHOLDER-->', str).
+        replace('<!--INIT_HTML_PLACEHOLDER-->',`<script>window.__init_data=${dataStr}</script>`);
 }
     
  
